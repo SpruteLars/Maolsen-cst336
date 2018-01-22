b8313a25d83c1b44d0b6e48a54472b45a7a54eb3
Marcus Olsen <maolsen@csumb.edu>
2018-01-22 17:27:18 +0000

    setup

---
+++ Maolsen/labs/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+         <a href="index.php">home</a>
+        <a href="about.php">about</a>
+        <a href="contact.php">Contact</a>
+    </body>
+</html>
